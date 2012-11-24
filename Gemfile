88522cc4659960ff82f5e84875c82316bf4eebd3
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-24 17:58:29 +0900

    定数などをいれるファイルを作成

--- Gemfile
+++ Gemfile
@@ -22,6 +22,13 @@ end
 
 gem 'jquery-rails'
 
+#oauth2ライブラリ
+gem "oauth2"
+
+#設定ファイルをつくるライブラリ
+gem 'settingslogic' 
+
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+#ログインやログアウトするコントローラー
+class SessionsController < ApplicationController
+
+  #oauth認証をするアクション。ここで、Facebookにリダイレクトする。
+  def get
+    
+    #oauthクラスをつくるための変数
+    
+    
+    
+    
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+end
