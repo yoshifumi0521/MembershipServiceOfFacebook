2a89ed694a40c9baa6c934e563bb907eff1adc97
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-24 16:41:17 +0900

    Railsアプリを作成した。testコードは外した。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MembershipServiceOfFacebook::Application.config.secret_token = '1b7a7a2898f505dfa3049c8d853167ccd2120bf79768e43340f8c0376fb539fcba3d7630f42672ac1eb6301cb91b95015a9aa28d550a7afc162a128ca2af2e2d'
